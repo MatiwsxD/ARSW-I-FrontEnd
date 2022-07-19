0fac151689bca23d2b1dcc3ea9bc3147d15e3062
mateomejia401@gmail.com <mateomejia401@gmail.com>
2022-07-19 12:42:45 -0500

    help

---
+++ src/pages/Game.js
@@ -0,0 +1,209 @@
+import "./Game.css"
+import { useState} from "react";
+import {over} from 'stompjs';
+import SockJS from 'sockjs-client';
+import { useEffect} from "react";
+
+var infoPlayer = ["","",""];
+var player = null
+var oponent = ["","",""]
+var turn = null
+var sala = null
+var stompClient = null
+var tempBoard =["","","","","","","","",""]
+const patterns =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+export let Game = function(){
+    var [pl, setPlayer] = useState(infoPlayer[0])
+    var [plw, setPlayerw] = useState(infoPlayer[1])
+    var [pll, setPlayerl] = useState(infoPlayer[2])
+    var [op, setOPlayer] = useState(oponent[0])
+    var [opw, setPlayerwo] = useState(oponent[1])
+    var [opl, setPlayerlo] = useState(oponent[2])
+    var [board, setBoard] = useState(tempBoard)
+    const[result,setResult] = useState({winner:"none",state:"none"})
+
+    useEffect(() => {
+        getInfoPlayer();
+        getData();
+        socketWeb();
+     }, []);
+
+    useEffect(()=>{
+        checkWin();
+    },[board])
+
+    useEffect(()=>{
+        if(result.state !="none"){
+            alert('Game Finished ' + result.winner);
+            //llamar una funcion que vea si el winner conside con el player, si si hacer fetch a winner, sino hacer fetch a losser
+        }
+       
+    },[result])
+
+
+    async function getInfoPlayer(){
+        let correo = sessionStorage.getItem("Correo");
+        let url2 ='https://peaceful-earth-72357.herokuapp.com/tictac/getUser/'+correo
+        let players = await fetch(url2,{
+            method: 'GET'
+            }).then(response => response.json());
+        infoPlayer = [players.name,players.pGanadas,players.pPerdidas];
+
+        stompClient.subscribe('/events/ws/'+sala+player, function(x){
+            oponent =  x.body.split(',')
+            setPlayer(infoPlayer[0])
+            setPlayerw(infoPlayer[1])
+            setPlayerl(infoPlayer[2])
+            setOPlayer(oponent[0])
+            setPlayerwo(oponent[1])
+            setPlayerlo(oponent[2])
+
+            if(parseInt(player) == 1){
+                stompClient.send('/events/ws/'+sala+2,{},infoPlayer)
+            }
+        
+        })
+        if(parseInt(player) == 2){
+            stompClient.send('/events/ws/'+sala+1,{},infoPlayer)
+        }
+
+        
+    }
+
+
+
+
+    async function getData(){
+        sala = sessionStorage.getItem("Sala")
+        //console.log(sala);
+        let url = 'https://peaceful-earth-72357.herokuapp.com/tictac/players/'+sala
+        let players = await fetch(url,{
+            method: 'GET'
+            }).then(response => response.json());
+        player = players;
+        if(players === 1){
+           turn  = true;
+        }
+        else{
+            turn  = false;
+        }
+    };
+
+    async function socketWeb(){
+        let Sock = await new SockJS("https://peaceful-earth-72357.herokuapp.com/tictac/tictactoe")
+        stompClient = over(Sock);
+        stompClient.connect({},function(){
+            stompClient.subscribe('/events/ws/'+sala, function(x){
+                let y = x.body.split(',').map(function(item) {
+                    return parseInt(item, 10);
+                });
+                turn  = !turn 
+                chooseSquare(y[0],y[1])
+                //console.log(y)                
+           }); 
+        });
+    };
+    
+
+    let chooseSquare = function(player1, square){
+        
+        setBoard(
+            board.map((val,idx)=>{
+                if(idx == square && val ==""){
+                    tempBoard[square] = player1
+                    setBoard(tempBoard)
+                    return player1
+                }
+                return val
+            })
+        )
+        
+
+    }
+    let sendMessage = function(pos){
+        if(turn){
+            stompClient.send('/events/ws/'+sala,{},[player,pos])
+        }
+        
+    }
+
+    const checkWin =() =>{
+        patterns.forEach((currPattern) =>{
+            const firstPlayer = board[currPattern[0]]
+            let foundWinningPattern = true
+            if(firstPlayer =="")return;
+            currPattern.forEach((idx)=>{
+                if(board[idx]!=firstPlayer){
+                    foundWinningPattern =false
+                }
+            })
+            if(foundWinningPattern){
+                setResult({winner:firstPlayer,state:"won"})
+
+            }
+        })
+    }
+
+    return(
+        <div className="App">
+            <div className="board">
+                <div className="row">
+                    <Square val={board[0]} chooseSquare={()=>sendMessage(0)}/>
+                    <Square val={board[1]} chooseSquare={()=>sendMessage(1)}/>
+                    <Square val={board[2]} chooseSquare={()=>sendMessage(2)}/>
+                </div>
+                <div className="row">
+                    <Square val={board[3]} chooseSquare={()=>sendMessage(3)}/>
+                    <Square val={board[4]} chooseSquare={()=>sendMessage(4)}/>
+                    <Square val={board[5]} chooseSquare={()=>sendMessage(5)}/>
+                </div>
+
+                <div className="row">
+                    <Square val={board[6]} chooseSquare={()=>sendMessage(6)}/>
+                    <Square val={board[7]} chooseSquare={()=>sendMessage(7)}/>
+                    <Square val={board[8]} chooseSquare={()=>sendMessage(8)}/>
+                </div>
+
+
+            </div>
+            <div>
+                    <Player jugador={pl} win={plw} lose={pll}/>
+                    <Player jugador={op} win={opw} lose={opl}/>
+                    
+                </div>
+        </div>
+        
+
+
+
+    );
+
+}
+
+let Square = function({val, chooseSquare}){
+    return (
+        <div className="square" onClick={chooseSquare}>
+            {val}
+        </div>
+    )
+}
+
+let Player = function({jugador,win,lose}){
+    return (
+        <div>
+            <br></br>
+            <h5>
+                No esta{jugador}
+            </h5>
+            <br></br>
+            <h5>
+                Hey{win}
+            </h5>
+            <br></br>
+            <h5>
+                cargando{lose}
+            </h5>
+        </div>
+    )
+}
---
+++ src/pages/Registerr.js
@@ -0,0 +1,84 @@
+import React, {useState} from 'react';
+import './register.css'
+import { useNavigate } from 'react-router-dom';
+export let Registerr= function() {
+
+    let [userName, setUserName] = useState('');
+    let [email, setEmail] = useState('');
+    let [password, setPassword] = useState('');
+    let [confirmPassword, setcPassword] = useState('');
+    let navigate = useNavigate();
+
+    let checkCampos = function(){
+        if(userName === '' ||  email ==='' || password === '' || confirmPassword === ''){
+            console.log('Debe llenar todos los campos') //Modificar con una alerta
+        }
+        
+        isRegistered();
+
+        
+    }
+
+    let isRegistered = async()=>{
+        let url = 'https://peaceful-earth-72357.herokuapp.com/tictac/checkUser/'+email
+        let data = await fetch(url,{
+            method: 'GET'
+          }).then(response => response.json());
+        if(!data){
+            submit();
+            alert("El usuario se registro correctamente")
+        }
+        else{
+            alert("El usuario ya se encuentra registrado")
+        }
+
+    }
+
+    let submit= async()=>{
+
+        let url = 'https://peaceful-earth-72357.herokuapp.com/tictac/registry/'+userName+'/'+email+'/'+password;
+        
+        await fetch(url,{
+            mode: 'no-cors',
+            method: 'POST'
+            });
+
+    }
+
+    let iniciarSesion = function(){
+       
+        navigate('/login')
+    }
+
+    
+
+
+
+    return(
+      <div className="form">
+          <div className="form-body">
+              <div className="lastname">
+                  <label className="form__label" htmlFor="userName">NickName</label>
+                  <input  type="text" name="" id="lastName"  className="form__input"placeholder="LastName" onChange={e=>setUserName(e.target.value)}/>
+              </div>
+              <div className="email">
+                  <label className="form__label" htmlFor="email">Email </label>
+                  <input  type="email" id="email" className="form__input" placeholder="Email" onChange={e=>setEmail(e.target.value)}/>
+              </div>
+              <div className="password">
+                  <label className="form__label" htmlFor="password">Password </label>
+                  <input className="form__input" type="password"  id="password" placeholder="Password" onChange={e=>setPassword(e.target.value)}/>
+              </div>
+              <div className="confirm-password">
+                  <label className="form__label" htmlFor="confirmPassword">Confirm Password </label>
+                  <input className="form__input" type="password" id="confirmPassword" placeholder="Confirm Password"onChange={e=>setcPassword(e.target.value)}/>
+              </div>
+          </div>
+          <div className="footer">
+              <button type="submit" className="btn" onClick={checkCampos}>Register</button>
+              <button type="submit" className="btn" onClick={iniciarSesion}>Iniciar Sesion</button>
+              
+          </div>
+      </div>      
+    )       
+}
